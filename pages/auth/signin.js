1d88fe75472df5f1a5d23eb872be48d8634b8906
pb.liu <liuhoom@gmail.com>
2024-11-28 13:50:40 +0800

    ci: dubble times

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -6,7 +6,7 @@ import Header from '@/components/header'
 import { getProviders, signIn } from 'next-auth/react'
 
 export default function Signin({ providers }) {
-  if (!data) {
+  if (!providers) {
     return <div>Loading...</div>
   }
 
